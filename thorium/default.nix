950fdce068e16acbf14ec9a4e6f8d96e11ffde00
Gandhuttt <gandhidhuta123123@gmail.com>
2025-06-15 02:42:37 +0700

    nyolong punya orang

--- thorium/default.nix
+++ thorium/default.nix
@@ -1,32 +1,3 @@
-{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }, appimageTools ? pkgs.appimageTools, fetchurl ? pkgs.fetchurl }:
+{ callPackage }:
 
-let
-  pname = "thorium";
-  version = "130.0.6723.174";
-
-  src = fetchurl {
-    url = "https://github.com/Alex313031/thorium/releases/download/M${version}/Thorium_Browser_${version}_AVX2.AppImage";
-    hash = "sha256-Ej7OIdAjYRmaDlv56ANU5pscuwcBEBee6VPZA3FdxsQ=";
-  };
-
-  app = appimageTools.wrapType2 {
-    inherit pname version src;
-  };
-
-  desktopItem = pkgs.makeDesktopItem {
-    name = "thorium";
-    exec = "${app}/bin/thorium --ozone-platform=wayland %U";
-    icon = "${./thorium.png}";
-    desktopName = "Thorium";
-    comment = "Privacy-focused Chromium fork";
-    categories = [ "Network" "WebBrowser" ];
-    mimeTypes = ["text/html" "x-scheme-handler/http" "x-scheme-handler/https"]; 
-    startupWMClass = "thorium"; # <--- THIS is important
-  };
-
-in
-
-pkgs.symlinkJoin {
-  name = "thorium";
-  paths = [ app desktopItem ];
-}
+callPackage ./thorium.nix { }
