cd5dadb479007238ff35bf0a751058254f8bbd91
Gandhuttt <gandhidhuta123123@gmail.com>
2025-06-15 17:05:44 +0700

    pake callPackage

--- default.nix
+++ default.nix
@@ -1,52 +1,57 @@
-{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; }}:
+{ pkgs ? import <nixpkgs> { system = builtins.currentSystem; } }:
+
+let
+ callPackage = pkgs.callPackage;
+in
 {
   
-  thorium = with pkgs; with pkgs.xorg; import ./thorium/thorium.nix { inherit 
-    lib 
-    libgbm
-    stdenv 
-    fetchurl 
-    wrapGAppsHook 
-    dpkg 
-    alsa-lib 
-    at-spi2-atk 
-    at-spi2-core 
-    atk 
-    cairo 
-    cups 
-    dbus 
-    expat 
-    fontconfig 
-    freetype 
-    gdk-pixbuf 
-    glib 
-    gtk3 
-    libX11 
-    libXScrnSaver 
-    libXcomposite 
-    libXcursor 
-    libXdamage 
-    libXext 
-    libXfixes 
-    libXi 
-    libXrandr 
-    libXrender 
-    libXtst 
-    libdrm 
-    libnotify 
-    libpulseaudio 
-    libuuid 
-    libxcb 
-    libxshmfence 
-    mesa 
-    nspr 
-    nss 
-    pango 
-    udev 
-    xdg-utils 
-    libxkbcommon 
-    makeWrapper
-  };
+  thorium = callPackage ./thorium/thorium.nix;
+  # thorium = with pkgs; with pkgs.xorg; import ./thorium/thorium.nix { inherit 
+  #   lib 
+  #   libgbm
+  #   stdenv 
+  #   fetchurl 
+  #   wrapGAppsHook 
+  #   dpkg 
+  #   alsa-lib 
+  #   at-spi2-atk 
+  #   at-spi2-core 
+  #   atk 
+  #   cairo 
+  #   cups 
+  #   dbus 
+  #   expat 
+  #   fontconfig 
+  #   freetype 
+  #   gdk-pixbuf 
+  #   glib 
+  #   gtk3 
+  #   libX11 
+  #   libXScrnSaver 
+  #   libXcomposite 
+  #   libXcursor 
+  #   libXdamage 
+  #   libXext 
+  #   libXfixes 
+  #   libXi 
+  #   libXrandr 
+  #   libXrender 
+  #   libXtst 
+  #   libdrm 
+  #   libnotify 
+  #   libpulseaudio 
+  #   libuuid 
+  #   libxcb 
+  #   libxshmfence 
+  #   mesa 
+  #   nspr 
+  #   nss 
+  #   pango 
+  #   udev 
+  #   xdg-utils 
+  #   libxkbcommon 
+  #   makeWrapper
+  # ;};
 
-  xpad = with pkgs; import ./xpad/xpad.nix { inherit lib stdenv fetchFromGitHub kernel };
+  xpad = callPackage ./xpad/xpad.nix;
 }
