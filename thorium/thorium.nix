3906362bfe6663af0ccd822714623de0e0020a7b
Gandhuttt <gandhidhuta123123@gmail.com>
2025-06-16 20:12:12 +0700

    fix icon lagi

--- thorium/thorium.nix
+++ thorium/thorium.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  libgbm,
   stdenv,
   fetchurl,
   wrapGAppsHook,
@@ -43,20 +42,7 @@
   xdg-utils,
   libxkbcommon,
   makeWrapper,
-  makeDesktopItem,
 }:
-
-let
-  desktopItem = makeDesktopItem {
-    name = "thorium";
-    exec = "thorium %U";
-    icon = "thorium"; # or use a full path like: "${placeholder "out"}/opt/chromium.org/thorium/product_logo_256.png"
-    comment = "Chromium fork with various patches and optimizations";
-    desktopName = "Thorium Browser";
-    categories = [ "Network" "WebBrowser" ];
-    mimeTypes = [ "x-scheme-handler/http" "x-scheme-handler/https" ];
-  };
-in
 
 stdenv.mkDerivation rec {
   pname = "thorium";
@@ -71,7 +57,6 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    libgbm
     alsa-lib
     at-spi2-atk
     at-spi2-core
@@ -114,44 +99,58 @@ stdenv.mkDerivation rec {
     dpkg-deb --fsys-tarfile $src | tar -x --no-same-permissions --no-same-owner
   '';
 
+  # No need to patch files that don't exist yet
   dontPatchELF = true;
 
   installPhase = ''
     runHook preInstall
-
     mkdir -p $out/bin $out/opt/chromium.org/thorium
-
     if [ -d ./opt/chromium.org/thorium ]; then
       cp -a ./opt/chromium.org/thorium/* $out/opt/chromium.org/thorium/
     elif [ -d ./opt/thorium ]; then
       cp -a ./opt/thorium/* $out/opt/chromium.org/thorium/
     fi
-
     makeWrapper "$out/opt/chromium.org/thorium/thorium" "$out/bin/thorium" \
       --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
       --prefix PATH : "${xdg-utils}/bin" \
       --add-flags "--no-sandbox"
-
     if [ -f $out/opt/chromium.org/thorium/chromedriver ]; then
       makeWrapper "$out/opt/chromium.org/thorium/chromedriver" "$out/bin/chromedriver" \
         --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}"
     fi
-
     if [ -f $out/opt/chromium.org/thorium/thorium-shell ]; then
       makeWrapper "$out/opt/chromium.org/thorium/thorium-shell" "$out/bin/thorium-shell" \
         --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
         --add-flags "--no-sandbox"
     fi
-
-    for size in 16 24 32 48 64 128 256; do
-      mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps
-      cp opt/chromium.org/thorium/product_logo_''${size}.png $out/share/icons/hicolor/''${size}x''${size}/apps/thorium.png || true
-    done
-
-
-    # Replace manual desktop file logic with makeDesktopItem
-    mkdir -p $out/share/applications
-    cp -r ${desktopItem}/share/applications/* $out/share/applications/
+    if [ -d ./usr/share/applications ]; then
+      mkdir -p $out/share/applications
+      cp ./usr/share/applications/thorium*.desktop $out/share/applications/ || true
+      
+      for file in $out/share/applications/*.desktop; do
+        if [ -f "$file" ]; then
+          substituteInPlace $file \
+            --replace "/opt/chromium.org/thorium/thorium" "$out/bin/thorium" \
+            --replace "/opt/chromium.org/thorium/" "$out/opt/chromium.org/thorium/" \
+            --replace "/opt/thorium/thorium" "$out/bin/thorium" \
+            --replace "/opt/thorium/" "$out/opt/chromium.org/thorium/" \
+            --replace "Exec=/usr/bin/thorium-browser" "Exec=thorium" \
+            --replace "StartupWMClass=thorium" "StartupWMClass=thorium-browser"
+        fi
+      done
+    fi
+    
+if [ -d ./usr/share/icons ]; then
+  mkdir -p "$out/share/icons"
+  cp -r ./usr/share/icons/* "$out/share/icons/" || true
+else
+  for i in 16 24 32 48 64 128 256; do
+    if [ -f "$out/opt/chromium.org/thorium/product_logo_''${i}.png" ]; then
+      mkdir -p "$out/share/icons/hicolor/''${i}x''${i}/apps"
+      cp "$out/opt/chromium.org/thorium/product_logo_''${i}.png" "$out/share/icons/hicolor/''${i}x''${i}/apps/thorium.png"
+    fi
+  done
+fi
 
     runHook postInstall
   '';
